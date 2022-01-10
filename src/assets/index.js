af4c2a072666233c50b36dc4788330618e46ca42
Mohammad Ashraful Kabir <maksumon007@gmail.com>
2022-01-11 01:20:13 +0800

    Added image assets and created index.js file with the references

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export const ESSENTIAL_HEALTH_SCREENING = require('./images/essential_health_screening.jpeg');
+export const ADVANCE_HEALTH_SCREENING = require('./images/advance_health_screening.jpeg');
+export const NO_SUGAR = require('./images/no_sugar.jpg');
+export const FIVE_K_RUN_WALK = require('./images/5k_run_walk.jpeg');
+export const EIGHT_GLASSES = require('./images/8_glasses.jpeg');
